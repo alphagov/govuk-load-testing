5a68c4472b1b4299deafac9e6a113d58338648a6
Michael Walker <mike@barrucadu.co.uk>
2018-10-18 10:20:23 +0100

    Add 'ratelimittoken' option, to forward to nginx
    We use these tokens to selectively bypass rate limiting, which vastly
    reduces the nmber of IP addresses we'll need:
    
    https://github.com/alphagov/govuk-puppet/blob/master/modules/router/templates/rate-limiting.conf.erb

--- test-plans/Frontend.scala
+++ test-plans/Frontend.scala
@@ -8,14 +8,20 @@ class Frontend extends Simulation {
   val baseUrl = System.getProperty("baseurl")
   val username = System.getProperty("username")
   val password = System.getProperty("password")
+  val rateLimitToken = System.getProperty("ratelimittoken")
   val users = Integer.getInteger("users", 1)
   val ramp = Integer.getInteger("ramp", 0)
+
+  val extraHeaders = Map(
+    "Rate-Limit-Token" -> rateLimitToken
+  )
 
   val httpProtocol = http
     .baseUrl(baseUrl)
     .acceptHeader("text/html")
     .acceptEncodingHeader("gzip, deflate")
     .basicAuth(username, password)
+    .headers(extraHeaders)
 
   val frontend = scenario("Frontend")
     .exec(http("homepage").get("/"))
