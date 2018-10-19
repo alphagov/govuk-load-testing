50d1bd4adf64beb622524e0bba834079f4e1af4c
Michael Walker <mike@barrucadu.co.uk>
2018-10-19 12:31:30 +0100

    Do not check for govuk:content-id meta tag
    Not all pages have this tag, for example /contact and
    /government/how-government-works.  These are both "special_route"
    documents, where the corresponding content item is empty (beyond a
    dummy content ID and the route).
    
    Making apps talk to the content store to find out the (unimportant!)
    content ID of a special_route page feels like a needlessly complex
    change for very little benefit.

--- test-plans/Simulation.scala
+++ test-plans/Simulation.scala
@@ -39,8 +39,7 @@ abstract class Simulation extends scenario.Simulation {
       .get(path + (if (bust) "?cachebust=" + cachebust else ""))
       .check(
         status.in(200 to 299),
-        regex("govuk:rendering-application").count.is(1),
-        regex("govuk:content-id").count.is(1)
+        regex("govuk:rendering-application").count.is(1)
       )
 
   def run(scn: ScenarioBuilder) =
