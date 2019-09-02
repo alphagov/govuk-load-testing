44665fa1ebfc863d95e6cd39564e41abc929e2cd
Deborah Chua <deborah.chua@digital.cabinet-office.gov.uk>
2019-09-02 14:55:11 +0100

    [WIP] Add duration time to simulation plan
    Adding a `duration` parameter allows us to specify how long the test
    runs for. If it is not set it will run with no time limitation.
    
    Paired with @edwardkerry

--- src/test/scala/govuk/DynamicLists.scala
+++ src/test/scala/govuk/DynamicLists.scala
@@ -5,6 +5,7 @@ import io.gatling.http.Predef._
 
 class DynamicLists extends Simulation {
   val factor = sys.props.getOrElse("factor", "1").toFloat
+  val duration = sys.props.getOrElse("duration", "0").toInt
 
   val paths = csv(dataDir + java.io.File.separatorChar + "get-ready-brexit-check_paths.csv").readRecords
 
@@ -21,5 +22,23 @@ class DynamicLists extends Simulation {
           }
       }
 
-  run(scn)
+
+  val scn2 =
+    scenario("DynamicLists")
+      .during(duration, "Soak test"){
+        feed(cachebuster)
+        .foreach(paths, "path") {
+          exec(flattenMapIntoAttributes("${path}"))
+            .repeat(session => math.ceil(session("hits").as[Int] * scale).toInt,
+                    "hit") {
+              exec(get("${base_path}", "${cachebust}-${hit}"))
+            }
+        }
+      }
+
+  if(duration > 0){
+    run(scn2)
+  } else{
+    run(scn)
+  }
 }
