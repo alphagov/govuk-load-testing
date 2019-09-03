77be9d877a59d8e33963009ee6b9adaaba2d0924
Mahmud Hussain <mahmud.hussain@digital.cabinet-office.gov.uk>
2019-09-03 14:42:19 +0100

    Add load test script for dynamic list email subscriptions
    As part of preparation for Brexit, we need to load test
    dynamic lists email subscriptions, which help users be informed of
    updates and changes to Brexit guidance.
    
    Trello: https://trello.com/c/qM2a6irn/1231-5-create-load-testing-script-for-dynamic-list-email-subscriptions
    
    Co-Authored-By: Alan Gabbianelli <alan.gabbianelli@digital.cabinet-office.gov.uk>

---
+++ src/test/scala/govuk/DynamicListsEmailSignup.scala
@@ -0,0 +1,68 @@
+package govuk
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+class DynamicListsEmailSignup extends Simulation {
+  val factor = sys.props.getOrElse("factor", "1").toFloat
+
+  val paths = csv(dataDir + java.io.File.separatorChar + "get-ready-brexit-check-email-signup_paths.csv").readRecords
+
+  val scale = factor / workers
+
+  val scn =
+    scenario("DynamicListsEmailSignup")
+      .feed(cachebuster)
+      .foreach(paths, "path") {
+        exec(flattenMapIntoAttributes("${path}"))
+          .repeat(session => math.ceil(session("hits").as[Int] * scale).toInt, "hit") {
+            exec(
+              get("${base_path}", "${cachebust}-${hit}")
+                .check(
+                  css("#checklist-email-signup", "action").saveAs("subscribeFormAction"),
+                  css("#checklist-email-signup input[name=authenticity_token]", "value").saveAs("subscribeAuthToken")
+                )
+                .check(status.is(200))
+            )
+              .exec(
+                http("POST Subscribe")
+                  .post("""${subscribeFormAction}""")
+                  .formParam("authenticity_token", """${subscribeAuthToken}""")
+                  .check(
+                    css(".checklist-email-signup", "action").saveAs("frequencyLink"),
+                    css(".checklist-email-signup input[name=topic_id]", "value").saveAs("emailSubscriptionFrequencyTopicId"),
+                    css(".checklist-email-signup input[name=authenticity_token]", "value").saveAs("subscribeAuthToken")
+                  )
+                  .check(status.is(200))
+              )
+              .exec(
+                http("POST Frequency")
+                  .post("""${frequencyLink}""")
+                  .formParam("authenticity_token", """${subscribeAuthToken}""")
+                  .formParam("topic_id", """${emailSubscriptionFrequencyTopicId}""")
+                  .formParam("frequency", "immediately")
+                  .check(
+                    css(".checklist-email-signup", "action").saveAs("emailSubscriptionFrequency"),
+                    css(".checklist-email-signup input[name=topic_id]", "value").saveAs("emailSubscriptionFrequencyTopicId"),
+                    css(".checklist-email-signup input[name=authenticity_token]", "value").saveAs("subscribeAuthToken")
+                  )
+                  .check(status.is(200))
+              )
+              .exec(
+                http("POST Email address")
+                  .post("""${emailSubscriptionFrequency}""")
+                  .formParam("authenticity_token", """${subscribeAuthToken}""")
+                  .formParam("topic_id", """${emailSubscriptionFrequencyTopicId}""")
+                  .formParam("frequency", "immediately")
+                  .formParam("address", "alice@example.com")
+                  .check(
+                    css(".govuk-panel__title")
+                  )
+                  .check(status.is(200))
+              )
+
+          }
+      }
+
+  run(scn)
+}
