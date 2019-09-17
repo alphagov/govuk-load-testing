6753321c61f28537d07c5eec4f426fb2847b3405
Alan Gabbianelli <alan.gabbianelli@digital.cabinet-office.gov.uk>
2019-09-17 12:10:04 +0100

    Revert "Rename email-alert-frontend session variables"
    This reverts commit f4bad0b2be5cb8a68bfb0be9330de494df70bf67.
    
    Co-Authored-By: Mahmud Hussain <mahmud.hussain@digital.cabinet-office.gov.uk>

--- src/test/scala/govuk/EmailAlertFrontend.scala
+++ src/test/scala/govuk/EmailAlertFrontend.scala
@@ -7,28 +7,28 @@ import io.gatling.http.Predef._
  * Email Alert Frontend scenario singleton object.
  *
  * Call 'exec(EmailAlertFrontend.subscribe)' to use in a Gatling scenario.
- * Requires 'emailAlertFrontendTopicId', 'emailAlertFrontendUrl', 'emailAlertFrontendAuthToken'
+ * Requires 'emailSubscriptionTopicId', 'frequencyLink', 'subscribeAuthToken'
  * to be present in the user session.
  */
 object EmailAlertFrontend {
   val subscribe = exec(
     http("Submit frequency")
-      .post("""${emailAlertFrontendUrl}""")
-      .formParam("authenticity_token", """${emailAlertFrontendAuthToken}""")
-      .formParam("topic_id", """${emailAlertFrontendTopicId}""")
+      .post("""${frequencyLink}""")
+      .formParam("authenticity_token", """${subscribeAuthToken}""")
+      .formParam("topic_id", """${emailSubscriptionTopicId}""")
       .formParam("frequency", "immediately")
       .check(
-        css(".checklist-email-signup", "action").saveAs("emailAlertFrontendEmailUrl"),
-        css(".checklist-email-signup input[name=topic_id]", "value").saveAs("emailAlertFrontendTopicId"),
-        css(".checklist-email-signup input[name=authenticity_token]", "value").saveAs("emailAlertFrontendAuthToken")
+        css(".checklist-email-signup", "action").saveAs("emailSubscriptionFrequency"),
+        css(".checklist-email-signup input[name=topic_id]", "value").saveAs("emailSubscriptionTopicId"),
+        css(".checklist-email-signup input[name=authenticity_token]", "value").saveAs("subscribeAuthToken")
       )
       .check(status.is(200))
     )
     .exec(
       http("Submit email address")
-        .post("""${emailAlertFrontendEmailUrl}""")
-        .formParam("authenticity_token", """${emailAlertFrontendAuthToken}""")
-        .formParam("topic_id", """${emailAlertFrontendTopicId}""")
+        .post("""${emailSubscriptionFrequency}""")
+        .formParam("authenticity_token", """${subscribeAuthToken}""")
+        .formParam("topic_id", """${emailSubscriptionTopicId}""")
         .formParam("frequency", "immediately")
         .formParam("address", "alice@example.com")
         .check(
