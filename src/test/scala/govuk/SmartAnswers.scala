321d76d1184c72036b4930decd352ac32f9828dd
Sean Rankine <sean.rankine@digital.cabinet-office.gov.uk>
2020-09-24 15:24:39 +0100

    Add smart answers load testing scenarios
    This changes adds a simulation to test the Smart Answers Coronavirus
    Find Support flow.
    
    Add a new run function to simulate a constant number of users.

---
+++ src/test/scala/govuk/SmartAnswers.scala
@@ -0,0 +1,49 @@
+package govuk
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+object LandingPage {
+  val go = exec(
+    http("Landing page")
+        .get("/coronavirus-find-support/s")
+        .check(status.is(200)))
+    .pause(1)
+}
+
+object NeedHelpWith {
+  val go = exec(
+    http("Question: need-help-with")
+        .get("/coronavirus-find-support/s/need-help-with/next")
+        .queryParam("response[]", "none")
+        .queryParam("next", "1")
+        .check(status.is(200)))
+    .pause(1)
+}
+
+object LeaveTheSite {
+  val go = exec(
+    http("Leave the site")
+      .get("/coronavirus-find-support/s/destroy_session")
+      .queryParam("ext_r", "true")
+      .check(status.is(200)))
+}
+
+class SmartAnswers extends Simulation {
+  val nodes = csv(dataDir + java.io.File.separatorChar + "smart-answers.csv").readRecords
+
+  val scn = scenario("Load test")
+    .exec(LandingPage.go)
+    .exec(NeedHelpWith.go)
+    .foreach(nodes, "node") {
+      exec(flattenMapIntoAttributes("${node}"))
+      .exec(http("Question: ${name}")
+        .get("/coronavirus-find-support/s/${name}/next")
+        .queryParam("response", "${response}")
+        .queryParam("next", "1"))
+      .pause(1)
+    }
+    .exec(LeaveTheSite.go)
+
+  runConstant(scn)
+}
