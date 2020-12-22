1c40a5cd93f7d0f8fc40ad4e81af5fe04be20c8b
Kevin Dew <kevindew@me.com>
2020-12-22 23:23:59 +0000

    Add a scenario for Coronavirus local restrictions
    This is a rushed together scenario for testing the
    /find-coronavirus-local-restrictions endpoint. I don't remotely know
    what I'm doing with Gatling so I put this together by copy and pasting
    an existing test and changing as little as possible.
    
    I've randomly generated a list of 100,000 postcodes for testing. I'm
    kinda hoping it doesn't choke on the + in the URL.

---
+++ src/test/scala/govuk/CoronavirusLocalRestrictions.scala
@@ -0,0 +1,40 @@
+package govuk
+
+import scala.io.Source
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+class CoronavirusLocalRestrictions extends Simulation {
+  val factor = sys.props.getOrElse("factor", "1").toFloat
+  val duration = sys.props.getOrElse("duration", "0").toInt
+
+  val paths = csv(dataDir + java.io.File.separatorChar + "coronavirus-local-restrictions.csv").readRecords
+
+  val scale = factor / workers
+
+  val scn =
+    scenario("CoronavirusLocalRestrictions")
+      .feed(cachebuster)
+      .foreach(paths, "path") {
+        exec(flattenMapIntoAttributes("${path}"))
+          .repeat(session => math.ceil(session("hits").as[Int] * scale).toInt,
+                  "hit") {
+            exec(get("${base_path}", "${cachebust}-${hit}"))
+          }
+      }
+
+  val scnWithDuration =
+    scenario("CoronavirusLocalRestrictions")
+      .during(duration, "Soak test") {
+        feed(cachebuster)
+        .foreach(paths, "path") {
+          exec(flattenMapIntoAttributes("${path}"))
+            .repeat(session => math.ceil(session("hits").as[Int] * scale).toInt,
+                    "hit") {
+              exec(get("${base_path}", "${cachebust}-${hit}"))
+            }
+        }
+      }
+
+  if (duration == 0) run(scn) else run(scnWithDuration)
+}
